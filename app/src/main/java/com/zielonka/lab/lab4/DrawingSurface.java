d22ac2898c4e024283a557542debb17e03eb19f6
Krystian Zielonka <s97779@pollub.edu.pl>
2024-05-08 09:27:47 +0200

    Lab4 big progress

--- app/src/main/java/com/zielonka/lab/lab4/DrawingSurface.java
+++ app/src/main/java/com/zielonka/lab/lab4/DrawingSurface.java
@@ -4,46 +4,76 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callback {
-    private final SurfaceHolder holder;
-//    private Thread drawingThread;
-//    private boolean threadWorking = false;
+import androidx.annotation.NonNull;
 
-    private Paint paint;
+public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callback, Runnable{
+    private final SurfaceHolder holder;
+    private Bitmap bitmap;
+    private Thread drawingThread;
+    private boolean threadWorking = false;
+    private Matrix identityMatrix;
+    Path path = new Path();;
+
+    private Paint pathPaint;
+    private Paint dotPaint;
     private Canvas canvas;
     public DrawingSurface(Context context, AttributeSet attrs) {
         super(context, attrs);
         holder = getHolder();
         holder.addCallback(this);
 
-        paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(2);
-        paint.setStyle(Paint.Style.FILL);
+        pathPaint = new Paint();
+        pathPaint.setAntiAlias(true);
+        pathPaint.setDither(true);
+        pathPaint.setColor(Color.BLUE);
+        pathPaint.setStyle(Paint.Style.STROKE);
+        pathPaint.setStrokeJoin(Paint.Join.ROUND);
+        pathPaint.setStrokeCap(Paint.Cap.ROUND);
+        pathPaint.setStrokeWidth(18);
+
+        dotPaint = new Paint(pathPaint);
+        dotPaint.setStyle(Paint.Style.FILL);
 
 
     }
 
-//    public void resumeDrawing() {
-//        drawingThread = new Thread(this);
-//        threadWorking = true;
-//        drawingThread.start();
-//    }
-//    public void pauseDrawing() {
-//        threadWorking = false;
-//    }
+    public void resumeDrawing() {
+        drawingThread = new Thread(this);
+        threadWorking = true;
+        drawingThread.start();
+    }
+    public void pauseDrawing() {
+        threadWorking = false;
+    }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         performClick();
         synchronized (DrawingSurface.this) {
-            canvas.drawCircle(event.getX(), event.getY(), 5, paint);
+            float X = event.getX();
+            float Y = event.getY();
+            switch (event.getActionMasked()) {
+                case MotionEvent.ACTION_DOWN:
+                    path.reset();
+                    path.moveTo(X, Y);
+                    canvas.drawCircle(X, Y, 25, dotPaint);
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    path.lineTo(X, Y);
+                    break;
+                case MotionEvent.ACTION_UP:
+                    canvas.drawCircle(X, Y, 25, dotPaint);
+                    canvas.drawPath(path, pathPaint);
+                    break;
+            }
         }
         return true;
     }
@@ -52,47 +82,46 @@ public class DrawingSurface extends SurfaceView implements SurfaceHolder.Callbac
         return super.performClick();
     }
 
-//    @Override
-//    public void run() {
-//        Log.i(DrawingSurface.class.getSimpleName(), "run");
-//
-//
-//        while (threadWorking) {
-//
-//            try {
-//                synchronized (holder) {
-//                    if (!holder.getSurface().isValid())
-//                        continue;
-//                    canvas = holder.lockCanvas(null);
-//                    synchronized (DrawingSurface.this) {
-//                        if (threadWorking) {
-//
-//                        }
-//                    }
-//                }
-//            } finally {
-//                if (canvas != null) {
-//                    holder.unlockCanvasAndPost(canvas);
-//                }
-//            }
-//            try {
-//                Thread.sleep(1000 / 25); // 25
-//            } catch (InterruptedException ignored) { }
-//        }
-//    }
+    @Override
+    public void run() {
+        Log.i(DrawingSurface.class.getSimpleName(), "run");
+
+        Canvas canvas1 = null;
+        while (threadWorking) {
+
+            try {
+                synchronized (holder) {
+                    if (!holder.getSurface().isValid())
+                        continue;
+                    canvas1 = holder.lockCanvas(null);
+                    synchronized (DrawingSurface.this) {
+                        if (threadWorking) {
+                            canvas1.drawBitmap(bitmap, identityMatrix, null);
+                            if(!path.isEmpty())
+                                canvas1.drawPath(path, pathPaint);
+                        }
+                    }
+                }
+            } finally {
+                if (canvas1 != null) {
+                    //TODO crashes when rotate
+                    holder.unlockCanvasAndPost(canvas1);
+                }
+            }
+            try {
+                Thread.sleep(1000 / 50); // 25
+            } catch (InterruptedException ignored) { }
+        }
+    }
 
     @Override
-    public void surfaceCreated(SurfaceHolder holder) {
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
         Log.i(DrawingSurface.class.getSimpleName(), "surface created");
-        Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+        bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
         canvas = new Canvas(bitmap);
-        canvas.drawARGB(255,255,255,255);
-
-        setOnTouchListener((view, event) -> {
-
-            return true;
-        });
-//        resumeDrawing();
+        canvas.drawARGB(255, 255, 255, 255);
+        identityMatrix = new Matrix();
+        resumeDrawing();
     }
 
     @Override
